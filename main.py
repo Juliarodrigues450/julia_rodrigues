e7eaed3b93befe170d039ec0bd71d38ca9376195
Julia Rodrigues Dos Santos - SENAI - Cuiaba/MT <00591952@alunosenai.mt>
2026-07-24 10:45:21 -0400

    numero

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ n1 = float (input("digite a primeira nota: "))
 n2 = float (input("digite a segunda nota: "))
 media = (n1 + n2 ) / 2
 print ("media: {media}")
+
+numero = int (input (digite um numero: ))
+print ("dobro: {numero * 2}")
