4dc3429a4b6c159d2712a98cf180a928e4546421
Julia Rodrigues Dos Santos - SENAI - Cuiaba/MT <00591952@alunosenai.mt>
2026-07-24 10:42:27 -0400

    nota

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print("Olá, {nome}!")
 
 idade = int(input("digite sua idade: "))
 print ("voce tem {idade} anos.")
+
+n1 = float (input("digite a primeira nota: "))
+n2 = float (input("digite a segunda nota: "))
+media = (n1 + n2 ) / 2
+print ("media: {media}")
